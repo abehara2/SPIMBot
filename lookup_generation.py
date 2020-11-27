82bfedd277698506ee3bf8b5ae7a6da3cc1dafaf
Matthew Walowski <mlw6@linux-a2.ews.illinois.edu>
2020-11-27 15:38:31 -0600

    Automatically pickup corn in the center area

--- lookup_generation.py
+++ lookup_generation.py
@@ -1,14 +1,20 @@
 import math
 
-TABLE_NAME = "atan"
-NUM_POINTS = 1000
+TABLE_NAME = "atan_table"
+BOARD_WIDTH = 320
 
 def generate_lookup_table():
     output = TABLE_NAME + ":\t.word\t"
-    for i in range(NUM_POINTS):
-        angle = (i * 2 * math.pi) / 100
-        v = math.atan(angle)
-        output += str(v) + "\t"
+
+    for i in range(BOARD_WIDTH + 1):
+        for j in range(BOARD_WIDTH + 1):
+            if (j == 0):
+                r = 0
+            else:
+                r = float(i) / j
+            v = int(round(math.atan(r) * 57.2958))
+            output += str(v)
+            output += "\t"
     
     print(output)
 
